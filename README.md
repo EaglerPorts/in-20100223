7c05d7edbf3754e1aa5fcebec2b49a9b0699b789
Colbster937 <colbster937@colbster937.dev>
2025-10-26 15:44:03 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Indev 20100223
+<img width="854" height="480" alt="image" src="https://raw.githubusercontent.com/EaglerPorts/in-20100223/refs/heads/main/img/mainmenu.png" />
---
+++ src/main/java/dev/colbster937/eaglercraft/EaglercraftVersion.java
@@ -0,0 +1,18 @@
+package dev.colbster937.eaglercraft;
+
+import net.lax1dude.eaglercraft.EagRuntime;
+
+public class EaglercraftVersion {
+	public static final String STORAGE_KEY = "_dev_colbster937_eaglercraft_in-20100223_";
+	public static final String EAGLER_VERSION = "25w44a";
+	public static final String MINECRAFT_VERSION = "in-20100223";
+	public static final String PROJECT_GITHUB = "https://github.com/EaglerPorts/in-20100223";
+	public static final String PROJECT_AUTHOR = "Colbster937";
+	public static final String PROJECT_BRAND = "colbster";
+	public static final String[] getTitleString() {
+		return new String[] {
+			"Eaglercraft " + EAGLER_VERSION + " [" + EagRuntime.getPlatformType() + "]",
+			"Made by " + PROJECT_AUTHOR
+		};
+	}
+}
